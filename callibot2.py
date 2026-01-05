cd31f96cd782768608ec53a3d62e2cc1feb43c4b
joris-bakker <T.joris.bakker@gmail.com>
2026-01-05 09:49:27 +0100

    Update callibot2.py

--- callibot2.py
+++ callibot2.py
@@ -8,18 +8,20 @@ import time
 # -------------------------------------------
 
 def motorL(dir, speed):
-    buf = bytearray(3)
-    buf[0] = 0x00   # linker Motor
-    buf[1] = dir    # 0=vorwärts, 1=rückwärts
-    buf[2] = min(255, max(0, speed))
-    i2c.write(0x20, buf)
+    if i2c.scan():
+        buf = bytearray(3)
+        buf[0] = 0x00   # linker Motor
+        buf[1] = dir    # 0=vorwärts, 1=rückwärts
+        buf[2] = min(255, max(0, speed))
+        i2c.write(0x20, buf)
 
 def motorR(dir, speed):
-    buf = bytearray(3)
-    buf[0] = 0x02   # rechter Motor
-    buf[1] = dir
-    buf[2] = min(255, max(0, speed))
-    i2c.write(0x20, buf)
+    if i2c.scan():
+        buf = bytearray(3)
+        buf[0] = 0x02   # rechter Motor
+        buf[1] = dir
+        buf[2] = min(255, max(0, speed))
+        i2c.write(0x20, buf)
 
 def stopL():
     motorL(0, 0) # Stoppe Linken Motor
@@ -32,24 +34,28 @@ def stopR():
 # -------------------------------------------
 
 def servo1(angle):
-    angle = max(0, min(180, angle))
-    i2c.write(0x20, bytearray([0x14, angle]))
+    if i2c.scan():
+        angle = max(0, min(180, angle))
+        i2c.write(0x20, bytearray([0x14, angle]))
 
 def servo2(angle):
-    angle = max(0, min(180, angle))
-    i2c.write(0x20, bytearray([0x15, angle]))
+    if i2c.scan():
+        angle = max(0, min(180, angle))
+        i2c.write(0x20, bytearray([0x15, angle]))
 
 # -------------------------------------------
 # LEDs am CalliBot
 # -------------------------------------------
 
 def ledL(on):
-    value = 1 if on else 0
-    i2c.write(0x21, bytearray([0x00, value]))
+    if i2c.scan():
+        value = 1 if on else 0
+        i2c.write(0x21, bytearray([0x00, value]))
 
 def ledR(on):
-    value = 2 if on else 0
-    i2c.write(0x21, bytearray([0x00, value]))
+    if i2c.scan():
+        value = 2 if on else 0
+        i2c.write(0x21, bytearray([0x00, value]))
 
 
 # -------------------------------------------
@@ -65,27 +71,35 @@ RGB = {
 }
 
 def rgbLed(led, r, g, b):  #  zum Beispiel "LV" als input eintragen
-    idx = RGB[led]
-    i2c.write(0x22, bytearray([0x03, idx, r, g, b]))
+    if i2c.scan():
+        idx = RGB[led]
+        i2c.write(0x22, bytearray([0x03, idx, r, g, b]))
 
 def rgbAll(r, g, b):
-    for idx in range(1, 5):
-        i2c.write(0x22, bytearray([0x03, idx, r, g, b]))
+    if i2c.scan():
+        for idx in range(1, 5):
+            i2c.write(0x22, bytearray([0x03, idx, r, g, b]))
 
 # -------------------------------------------
 # Sensorsystem
 # -------------------------------------------
 
 def bumperL():
-    val = i2c.read(0x21, 1)[0]
+    val = 0
+    if i2c.scan():
+        val = i2c.read(0x21, 1)[0]
     return bool(val & 0x08)
 
 def bumperR():
-    val = i2c.read(0x21, 1)[0]
+    val = 0
+    if i2c.scan():
+        val = i2c.read(0x21, 1)[0]
     return bool(val & 0x04)
 
 def lineL():
-    val = i2c.read(0x21, 1)[0]
+    val = 0
+    if i2c.scan():
+        val = i2c.read(0x21, 1)[0]
     return not bool(val & 0x02)   # 0 = dunkel, 1 = hell
 
 def lineR():
@@ -93,7 +107,9 @@ def lineR():
     return not bool(val & 0x01)
 
 def distance_mm():
-    buf = i2c.read(0x21, 3)
+    buf = [0,0,0]
+    if i2c.scan():
+        buf = i2c.read(0x21, 3)
     return buf[1] * 256 + buf[2]
 
 def distance_cm():
